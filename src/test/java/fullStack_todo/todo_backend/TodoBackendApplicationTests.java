7493c27990fd1d85ee1e39a2eb5e4b94b0df623f
sckat <ala_cl@yahoo.com>
2024-07-17 13:57:58 +0300

    init commit

---
+++ src/test/java/fullStack_todo/todo_backend/TodoBackendApplicationTests.java
@@ -0,0 +1,13 @@
+package fullStack_todo.todo_backend;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class TodoBackendApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
